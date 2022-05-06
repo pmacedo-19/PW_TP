f0e95f27eeb4cdabc1a6cf3873892877672e5ff7
João Dias <jonhybd@gmail.com>
2022-05-06 20:07:48 +0100

    1stish commit

--- JS/login.js
+++ JS/login.js
@@ -1,15 +1,9 @@
-function validate()
-{
-    var username=document.getElementById("username").value;
-    var password=document.getElementById("password").value;
-    if(username=="admin"&& password=="admin1")
-    {
-        alert("Login Succesfully");
-        
-        return false;
-    }
-    else
-    {
-        alert("Login Failed");
-    }
+function validate() {
+  var username = document.getElementById("username").value;
+  var password = document.getElementById("password").value;
+
+  if (username == "admin" && password == "admin1") {
+    alert("Login Succesfully");
+    return false;
+  } else alert("Login Failed");
 }
