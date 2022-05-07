02c23cc3f52d539c7e7ab67032154fc1a0aed9ac
pmacedo-19 <wardian@protonmail.com>
2022-05-07 15:25:38 +0100

    Login finish

--- JS/login.js
+++ JS/login.js
@@ -1,9 +1,16 @@
-function validate() {
-  var username = document.getElementById("username").value;
-  var password = document.getElementById("password").value;
+const loginForm = document.getElementById("login-form");
+const loginButton = document.getElementById("login-form-submit");
+const loginErrorMsg = document.getElementById("login-error-msg");
 
-  if (username == "admin" && password == "admin1") {
-    alert("Login Succesfully");
-    return false;
-  } else alert("Login Failed");
-}
+loginButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    const username = loginForm.username.value;
+    const password = loginForm.password.value;
+
+    if (username === "user" && password === "user123") {
+        alert("You have successfully logged in.");
+        window.location.href = '/html/home.html';
+    } else {
+        loginErrorMsg.style.opacity = 1;
+    }
+})
