5aa78fc6a3f6ddd3af2e91f0341c8aadffa6fadc
pmacedo-19 <wardian@protonmail.com>
2022-05-07 19:03:27 +0100

    Update register.js

--- JS/register.js
+++ JS/register.js
@@ -4,34 +4,6 @@ const usernameErrorMsg = document.getElementById("username-error-msg");
 const passwordErrorMsg = document.getElementById("password-error-msg");
 
 
-function validateUsername(fld) {
-    var error = "";
-    var illegalChars = /\W/; // allow letters, numbers, and underscores
- 
-    if (fld.value == "") {
-        fld.style.background = 'Yellow';
-        error = "You didn't enter a username.\n";
-        alert(error);
-        return true;
- 
-    } else if ((fld.value.length < 5) || (fld.value.length > 15)) {
-        fld.style.background = 'Yellow';
-        error = "The username is the wrong length.\n";
-		alert(error);
-		return true;
- 
-    } else if (illegalChars.test(fld.value)) {
-        fld.style.background = 'Yellow';
-        error = "The username contains illegal characters.\n";
-		alert(error);
-		return true;
- 
-    } else {
-        fld.style.background = 'White';
-    }
-    return false;
-}
-
 
 registerButton.addEventListener("click", (e) => {
     e.preventDefault();
@@ -40,14 +12,16 @@ registerButton.addEventListener("click", (e) => {
     const password = registerForm.password.value;
     const password2 = registerForm.password2.value;
 
-    var letters = /^[A-Za-z]+$/;
-
-    if ( password === password2 && validateUsername(username)){
-        alert("Registration Successfull")
+    if ( password === password2 && username.length > 5){
+        alert("Registration Successfull");
         window.location.href = '/html/home.html';
     }
-    else {
-        alert("Username Invalid or Passwords don't match!")
-    } 
+    else if (username.length < 6){
+        alert("Username Invalid!");
+    }
+    else
+    {
+        alert("Passwords Don't Match!");
+    }
     
 })
