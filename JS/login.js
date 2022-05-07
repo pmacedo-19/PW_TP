ba30cf1dd0b094715fc4d288ce941ee74bd38401
João Dias <jonhybd@gmail.com>
2022-05-07 21:49:06 +0100

    last update pre-deliver

--- JS/login.js
+++ JS/login.js
@@ -3,14 +3,12 @@ const loginButton = document.getElementById("login-form-submit");
 const loginErrorMsg = document.getElementById("login-error-msg");
 
 loginButton.addEventListener("click", (e) => {
-    e.preventDefault();
-    const username = loginForm.username.value;
-    const password = loginForm.password.value;
+  e.preventDefault();
+  const username = loginForm.username.value;
+  const password = loginForm.password.value;
 
-    if (username === "user" && password === "user123") {
-        alert("You have successfully logged in.");
-        window.location.href = '/html/home.html';
-    } else {
-        loginErrorMsg.style.opacity = 1;
-    }
-})
+  if (username === "user" && password === "user123") {
+    alert("You have successfully logged in.");
+    window.location.href = "/html/home.html";
+  } else loginErrorMsg.style.opacity = 1;
+});
